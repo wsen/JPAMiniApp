b622525ce24a1123068e30038b4ee41cf3cadadb
tlubowiecki <tlubowiecki@localhost>
2018-07-30 16:07:13 +0200

    Init

---
+++ src/de/gfn/jpa/App.java
@@ -0,0 +1,41 @@
+package de.gfn.jpa;
+
+import de.gfn.jsp.entity.Scout;
+import java.util.Date;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import static org.eclipse.persistence.sessions.SessionProfiler.Transaction;
+
+/**
+ *
+ * @author tlubowiecki
+ */
+public class App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("JPAMiniAppPU");
+        EntityManager em = emf.createEntityManager();
+        
+//        Scout s = new Scout();
+//        s.setFirstname("Peter");
+//        s.setLastname("Parker");
+//        s.setBirthdate(new Date());
+//        
+        em.getTransaction().begin();
+        //em.persist(s);
+        
+        Scout s = em.find(Scout.class, 1L);
+        s.setFirstname("Bob");
+        //System.out.println(s);
+        
+        em.getTransaction().commit();
+        em.close();
+        
+    }
+    
+}
---
+++ src/de/gfn/jsp/entity/Scout.java
@@ -0,0 +1,93 @@
+package de.gfn.jsp.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author tlubowiecki
+ */
+@Entity(name = "scouts")
+public class Scout implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    @Column(length = 50)
+    private String firstname;
+    
+    @Column(length = 50)
+    private String lastname;
+    
+    @Temporal(TemporalType.DATE)
+    private Date birthdate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+    
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Scout)) {
+            return false;
+        }
+        Scout other = (Scout) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "de.gfn.jsp.entity.Scout[ id=" + id + " ]";
+    }
+    
+}
